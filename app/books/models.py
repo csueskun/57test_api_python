83f63a965a4e1a2fb0014428b8eef30e9ab2d040
skn <csueskun@gmail.com>
2023-03-08 13:39:02 -0500

    update models

--- app/books/models.py
+++ app/books/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from ..connection import Base
+import datetime
 
 # define the book model
 class Book(Base):
@@ -29,10 +30,27 @@ class User(Base):
     email = Column(String)
     password = Column(String)
     books = relationship("Book", back_populates="user")
+    sessions = relationship("Session", back_populates="user")
 
     def __repr__(self) -> str:
-        return f"<Page(number={self.number}, book='{self.book.title}')>"
+        return f"<Page(email={self.email})>"
 
-    # define a method that returns the page data as a dictionary
+    # define a method that returns the user data as a dictionary
+    def as_dict(self) -> dict:
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+# define the session model
+class Session(Base):
+    __tablename__ = 'session'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=datetime.datetime.now)
+    token = Column(String)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", back_populates="sessions")
+
+    def __repr__(self) -> str:
+        return f"<Page(user={self.user_id}, created='{self.created_at}')>"
+
+    # define a method that returns the session data as a dictionary
     def as_dict(self) -> dict:
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
